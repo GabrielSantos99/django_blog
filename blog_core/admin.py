959e18adbcfea3e5b5d240bf9ba006bf92320385
Gabriel <ar.gabrielss@gmail.com>
2025-04-01 10:03:30 -0300

    Add ModelAdmin to Post

--- blog_core/admin.py
+++ blog_core/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Category, Post, Comment
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'published_at')
+    list_filter = ('status', 'published_at', 'author')
+    search_fields = ('title', 'content', 'summary')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = ('published_at')
+    ordering = ('-published_at',)
+    list_editable = ('status',)
+
 admin.site.register(Category)
-admin.site.register(Post)
 admin.site.register(Comment)
